073b23c61000b562e20f2c62c1f1a58438149c0d
Chen Tao <tabasco.chen@gmail.com>
2015-05-03 22:44:40 -0230

    add a wrapper to cython gradient function

--- walmart/cost.py
+++ walmart/cost.py
@@ -3,6 +3,7 @@ import pandas as pd
 from feature import compute_feature2
 from similarity import l_logistic_sim, sim_func, l_sim_func
 from utils import normalize
+import cost2
 
 def l_cost_fun(Y_hat, Y, l, train_num, alpha_train, alpha_unknown):
     Y_hat=Y_hat.reshape(Y.shape)
@@ -23,10 +24,10 @@ def l_fun_sim(Y_hat, l):
     n=Y_hat.shape[0]
     sim=0
     if len(Y_hat.shape)>1:
-        for i in range(n):         
+        for i in range(n):
             sim+=np.dot(l(i), Y_hat).dot(Y_hat[i])
     else:
-        for i in range(n):         
+        for i in range(n):
             sim+=np.dot(l(i), Y_hat)*Y_hat[i]
     return sim
 
@@ -34,7 +35,7 @@ def fun_sim(Y_hat, L):
     return np.trace(Y_hat.T.dot(L).dot(Y_hat))
 
 def fun_log_error(Y_hat, Y, train_num, alpha_train, alpha_unknown):
-    cost_Y_hat=fun_log_error_a(Y_hat, Y, train_num, alpha_train, alpha_unknown)    
+    cost_Y_hat=fun_log_error_a(Y_hat, Y, train_num, alpha_train, alpha_unknown)
     return np.sum(cost_Y_hat)
 
 def fun_log_error_a(Y_hat, Y, train_num, alpha_train, alpha_unknown):
@@ -69,8 +70,8 @@ def l_g_fun_sim(Y_hat, l):
     for i in range(n):
         g[i]=np.dot(l(i), Y_hat)
     return 2*g
-    
-def g_fun_sim(Y_hat, L):    
+
+def g_fun_sim(Y_hat, L):
     return 2*np.dot(L, Y_hat)
 
 def g_fun_log_error(Y_hat, Y, train_num, alpha_train, alpha_unknown):
@@ -108,13 +109,13 @@ def g_cost_fun3(hidden, hidden_shape, fmat, Y_hat, Y_shape, D):
     fmat[:,offset:]=hidden
     _, nm=normalize(fmat) # shape=nx1
     n= Y_shape[0]
-    g=np.zeros(hidden_shape)    
+    g=np.zeros(hidden_shape)
     Fmat=np.dot(fmat,fmat.T)
     for i in range(n):
         d0=D[i]
         c0=1/((nm[i]**2)*nm) # shape=nx1
-        c1=fmat[:, offset:] * nm[i] # shape=nxm        
-        c2=np.outer(Fmat[i]/nm[i], fmat[i, offset:]) # shape=nxm        
+        c1=fmat[:, offset:] * nm[i] # shape=nxm
+        c2=np.outer(Fmat[i]/nm[i], fmat[i, offset:]) # shape=nxm
         g[i]=np.sum((c1-c2)*(d0*c0)[:, np.newaxis], axis=0)  # shape
     return g.flatten()
 
@@ -132,12 +133,17 @@ def g_cost_fun5(fmat_weight, fmat, Y_hat, Y_shape, D):
     working_fmat=fmat_weight * fmat
     _, nm=normalize(working_fmat) # shape=nx1
     n= Y_shape[0]
-    g=np.zeros(fmat.shape)    
+    g=np.zeros(fmat.shape)
     Fmat=np.dot(working_fmat,working_fmat.T)
     for i in range(n):
         d0=D[i]
         c0=1/((nm[i]**2)*nm) # shape=nx1
-        c1=fmat[i] * working_fmat * nm[i] # shape=nxm        
-        c2=np.outer(Fmat[i]/nm[i], fmat[i]*working_fmat[i]) # shape=nxm        
+        c1=fmat[i] * working_fmat * nm[i] # shape=nxm
+        c2=np.outer(Fmat[i]/nm[i], fmat[i]*working_fmat[i]) # shape=nxm
         g[i]=np.sum((c1-c2)*(d0*c0)[:, np.newaxis], axis=0)  # shape
     return g.flatten()
+
+def g_cost_fun52(fmat_weight, fmat, Y_hat, Y_shape, D):
+    Y_hat=Y_hat.reshape(Y_shape)
+    fmat_weight=fmat_weight.reshape(fmat.shape)
+    return cost2.g_cost_fun5(fmat_weight, fmat, Y_hat, D).flatten()
